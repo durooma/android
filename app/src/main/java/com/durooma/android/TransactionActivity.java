b840cf5309bab79489d04816539df0f9bc9af34c
Hannes Widmoser <widmoser@gmail.com>
2017-03-18 13:36:50 +0100

    Setup project

---
+++ app/src/main/java/com/durooma/android/TransactionActivity.java
@@ -0,0 +1,13 @@
+package com.durooma.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+    }
+}
