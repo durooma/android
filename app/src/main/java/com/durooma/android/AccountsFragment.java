dc8c1bf40427bad1f244493fe668fa0a4265acc5
Hannes Widmoser <widmoser@gmail.com>
2017-03-30 10:59:19 +0200

    Outsource common list code

--- app/src/main/java/com/durooma/android/AccountsFragment.java
+++ app/src/main/java/com/durooma/android/AccountsFragment.java
@@ -1,158 +1,37 @@
 package com.durooma.android;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.view.*;
-import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import com.durooma.android.api.Api;
-import com.durooma.android.api.ApiLoader;
 import com.durooma.android.controller.AccountAdapter;
 import com.durooma.android.model.Account;
-import org.androidannotations.annotations.*;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EFragment;
 import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@EFragment(R.layout.fragment_accounts)
-@OptionsMenu(R.menu.fragment_list)
-public class AccountsFragment
-        extends Fragment
-        implements LoaderManager.LoaderCallbacks<List<Account>>,
-        AbsListView.MultiChoiceModeListener,
-        SwipeRefreshLayout.OnRefreshListener {
+@EFragment
+public class AccountsFragment extends ListFragment<Account> {
 
-    protected static final int STATUS_EMPTY = 0;
-    protected static final int STATUS_LIST = 1;
-    protected static final int STATUS_ERROR = 2;
-
-    protected static final int LOAD_ACCOUNTS = 0;
-
-    @ViewById
-    ListView list;
-
-    @ViewById
-    View empty;
-
-    @ViewById
-    View error;
-
-    @ViewById
-    FloatingActionButton fab;
-
-    @ViewById
-    SwipeRefreshLayout swipeRefresh;
-
-    private ArrayAdapter<Account> listAdapter;
-
-    private View[] statusViews;
-
-    @AfterViews
-    public void init() {
-        statusViews = new View[]{empty, list, error};
-        getLoaderManager().initLoader(LOAD_ACCOUNTS, null, this);
-        listAdapter = new AccountAdapter(getContext());
-        list.setAdapter(listAdapter);
-        list.setMultiChoiceModeListener(this);
-        swipeRefresh.setOnRefreshListener(this);
-        swipeRefresh.setRefreshing(true);
-    }
-
-    public void setStatus(int status) {
-        list.setVisibility(View.GONE);
-        empty.setVisibility(View.GONE);
-        error.setVisibility(View.GONE);
-        statusViews[status].setVisibility(View.VISIBLE);
+    @Override
+    protected ArrayAdapter<Account> onCreateAdapter(Context context) {
+        return new AccountAdapter(context);
     }
 
     @Override
-    public Loader<List<Account>> onCreateLoader(int id, Bundle args) {
-        return new ApiLoader<>(getContext(), Api.get().getAccounts());
+    protected Observable<List<Account>> getItems() {
+        return Api.get().getAccounts();
     }
 
     @Override
-    public void onLoadFinished(Loader<List<Account>> loader, List<Account> data) {
-        swipeRefresh.setRefreshing(false);
-        listAdapter.clear();
-        if (data != null) {
-            listAdapter.addAll(data);
-            setStatus(data.isEmpty() ? STATUS_EMPTY : STATUS_LIST);
-        } else {
-            setStatus(STATUS_ERROR);
-        }
-    }
-
-    @Override
-    public void onLoaderReset(Loader<List<Account>> loader) {
-    }
-
-    @Override
-    public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
-
-    }
-
-    @Override
-    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-        MenuInflater menuInflater = mode.getMenuInflater();
-        menuInflater.inflate(R.menu.edit_items, menu);
-        mode.setTitle(R.string.edit_accounts);
-        return true;
-    }
-
-    @Override
-    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-        return true;
-    }
-
-    @Override
-    public boolean onActionItemClicked(final ActionMode mode, MenuItem item) {
-        List<Observable<Void>> deleteOperations = new ArrayList<>();
-        for (Long id : list.getCheckedItemIds()) {
-            deleteOperations.add(Api.get().removeAccount(id));
-        }
-        Observable.merge(deleteOperations)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<Void>() {
-                    @Override
-                    public void call(Void aVoid) {
-                        getLoaderManager().restartLoader(LOAD_ACCOUNTS, null, AccountsFragment.this);
-                        mode.finish();
-                    }
-                });
-        return true;
-    }
-
-    @Override
-    public void onDestroyActionMode(ActionMode mode) {
+    protected Observable<Void> removeItem(long id) {
+        return Api.get().removeAccount(id);
     }
 
     @Click(R.id.fab)
     public void newAccount() {
         startActivity(new Intent(getContext(), EditAccountActivity_.class));
-    }
-
-    void refresh() {
-        getLoaderManager().restartLoader(LOAD_ACCOUNTS, null, this);
-    }
-
-    @OptionsItem(R.id.menu_refresh)
-    void menuRefresh() {
-        swipeRefresh.setRefreshing(true);
-        refresh();
-    }
-
-
-    @Override
-    public void onRefresh() {
-        refresh();
     }
 }
---
+++ app/src/main/java/com/durooma/android/ListFragment.java
@@ -0,0 +1,180 @@
+package com.durooma.android;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.*;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.durooma.android.api.ApiLoader;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ListFragment<T>
+        extends Fragment
+        implements LoaderManager.LoaderCallbacks<List<T>>,
+        AbsListView.MultiChoiceModeListener,
+        SwipeRefreshLayout.OnRefreshListener {
+
+    protected static final int STATUS_EMPTY = 0;
+    protected static final int STATUS_LIST = 1;
+    protected static final int STATUS_ERROR = 2;
+
+    protected static final int LOAD_ITEMS = 0;
+
+    ListView list;
+    View empty;
+    View error;
+    FloatingActionButton fab;
+    SwipeRefreshLayout swipeRefresh;
+
+    private ArrayAdapter<T> listAdapter;
+
+    private View[] statusViews;
+
+    protected abstract ArrayAdapter<T> onCreateAdapter(Context context);
+
+    protected abstract Observable<List<T>> getItems();
+
+    protected abstract Observable<Void> removeItem(long id);
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        list = (ListView)view.findViewById(R.id.list);
+        empty = view.findViewById(R.id.empty);
+        error = view.findViewById(R.id.error);
+        fab = (FloatingActionButton)view.findViewById(R.id.fab);
+        swipeRefresh = (SwipeRefreshLayout)view.findViewById(R.id.swipe_refresh);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        statusViews = new View[]{empty, list, error};
+        getLoaderManager().initLoader(LOAD_ITEMS, null, this);
+        listAdapter = onCreateAdapter(getContext());
+        list.setAdapter(listAdapter);
+        list.setMultiChoiceModeListener(this);
+        swipeRefresh.setOnRefreshListener(this);
+        swipeRefresh.setRefreshing(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_list, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                menuRefresh();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void setStatus(int status) {
+        list.setVisibility(View.GONE);
+        empty.setVisibility(View.GONE);
+        error.setVisibility(View.GONE);
+        statusViews[status].setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public Loader<List<T>> onCreateLoader(int id, Bundle args) {
+        return new ApiLoader<>(getContext(), getItems());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<T>> loader, List<T> data) {
+        swipeRefresh.setRefreshing(false);
+        listAdapter.clear();
+        if (data != null) {
+            listAdapter.addAll(data);
+            setStatus(data.isEmpty() ? STATUS_EMPTY : STATUS_LIST);
+        } else {
+            setStatus(STATUS_ERROR);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<T>> loader) {
+    }
+
+    @Override
+    public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+
+    }
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        MenuInflater menuInflater = mode.getMenuInflater();
+        menuInflater.inflate(R.menu.edit_items, menu);
+        mode.setTitle(R.string.edit_accounts);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onActionItemClicked(final ActionMode mode, MenuItem item) {
+        List<Observable<Void>> deleteOperations = new ArrayList<>();
+        for (Long id : list.getCheckedItemIds()) {
+            deleteOperations.add(removeItem(id));
+        }
+        Observable.merge(deleteOperations)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        getLoaderManager().restartLoader(LOAD_ITEMS, null, ListFragment.this);
+                        mode.finish();
+                    }
+                });
+        return true;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+    }
+
+    void refresh() {
+        getLoaderManager().restartLoader(LOAD_ITEMS, null, this);
+    }
+
+    void menuRefresh() {
+        swipeRefresh.setRefreshing(true);
+        refresh();
+    }
+
+
+    @Override
+    public void onRefresh() {
+        refresh();
+    }
+}
